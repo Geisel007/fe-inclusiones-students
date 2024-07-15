4a45631aa1e7778ff746c7b30af67f5aef52a5a3
geiisel <geisel.montoya@efika.solutions>
2024-07-15 12:22:42 -0600

    Add Page: List Request

---
+++ src/pages/ListRequest/ListRequest.jsx
@@ -0,0 +1,23 @@
+// React
+import React from 'react'
+// Components
+import View from '../../components/HtmlComponents/View/View'
+
+// Styles
+import './ListRequest.style.css'
+
+/**
+@file ListRequest page component
+*/
+const ListRequest = () => {
+
+  return (
+    <View 
+      className={'list-request-container'}
+    >
+      This is the ListRequest
+    </View>
+  )
+}
+
+export default ListRequest
